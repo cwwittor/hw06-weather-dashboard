cae1cf437c460eb229e892b714f1a0dcb9129db1
Christian Wittorp <cwwittor@ncsu.edu>
2021-01-22 00:26:07 -0500

    added comments and started updating the readME

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hw06-weather-dashboard
+
+Homework 6 for my bootcamp class
+
+Had to go over the alloted time due to unforseen family circumstances that required a lot of attention and time during the week, other than going over time I was really happy with the end result due to it working extremely well. I think the only thing that probably could've been updated is maybe a try/catch to handle cities that don't exist in the api or cities that are mispelled, but since we haven't covered that much in class I wasn't really sure how to implement so I just didn't do it since it wasn't in the requirements. At the start of the hw I had like everything in one class, but when I finally got more of the code alloted to different classes everything became a lot easier to visualize. The homework assignment was a weather dashboard this week where we had to use multiple APIs to collect different weather data and display it nicely to the screen. The website had to save the most recent city to local storage, get a 5 day forecast, have search history of cities previously searched, and display some data from the current day.
+
+The point I struggled with most for the project was initially having too much in the same function as well as some of the html/css stuff due to front end still being a little foreign to me. Unsure if I used conventional means but am happy with my results this hw assignment.
+
+
+https://cwwittor.github.io/hw05-WorkScheduler/
+
+![image](https://i.imgur.com/ErgCgL4.png)
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 
     startUp();
 
+//will update the page with local storage on startup
 function startUp(){
     if (cityName == null)   {
         cityName = "raleigh";
@@ -19,6 +20,7 @@ function startUp(){
     fiveDaySchedule();
 }
 
+//when search button is clicked it searchs for the city in the list
 $("#city-button").on("click", function() {
     
     cityName = $("#search-city").val();
@@ -30,6 +32,7 @@ $("#city-button").on("click", function() {
     
 });
 
+//when an item on the unordered list is clicked it will search the city
 $("ul").on("click", "li", function() {
 
     cityName = $(this).text();
@@ -42,17 +45,20 @@ $("ul").on("click", "li", function() {
 
 });
 
- 
+//function that updates the list of cities on the list on the left. Limiting it to 8 since I dont want too many
 function updateCitiesList () {
+    //if statement that checks if the city name is already in the list. If it is it will be removed to be put back into the list at the top after.
     if (arrayOfCities.includes(cityName.toLowerCase())) {
         var index = arrayOfCities.indexOf(cityName);
         arrayOfCities.splice(index, 1);
     }
-    arrayOfCities.unshift(cityName.toLowerCase());
-    $(".list-of-cities").empty();
+    arrayOfCities.unshift(cityName.toLowerCase()); //adds the new city to front of the list
+    $(".list-of-cities").empty(); //empties list as a whole for elements to be added back when iterating through the loop
+    //if there are more than 8 on the list they will be removed from bottom
     while (arrayOfCities.length > 8) {
         arrayOfCities.pop();
     }
+    //for loop for adding and appending cities to searched list on the left side
     for (var i = 0; i < arrayOfCities.length; i++) {
         var newCity = document.createElement("li");
         newCity.classList += "city-in-list list-group-item";
@@ -60,11 +66,11 @@ function updateCitiesList () {
         document.getElementById("unordered-list-cities").appendChild(newCity);
         $("#"+newCity.id).append(arrayOfCities[i]);
     }
-    localStorage.setItem('starter-city', cityName);
+    localStorage.setItem('starter-city', cityName); //saving latest city to local storage
     
 }
 
-
+//updates the forecast on the top right side of the screen with data pulled from 2 APIs
 function updateForecast () {
     $.ajax({
         url: queryURL,
@@ -77,6 +83,7 @@ function updateForecast () {
         var lati = pageInfo.coord.lat; 
         var nameOfCity = pageInfo.name;
 
+        //adding variables to HTML
         $(".temperature").empty();
         $(".temperature").append("Temperature: " + temperature + " °F");
         $(".humidity").empty();
@@ -84,7 +91,7 @@ function updateForecast () {
         $(".wind-speed").empty();
         $(".wind-speed").append("Wind Speed: " + windSpd + " MPH");
 
-
+        //building url for second api
         var queryURLuV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lati + "&lon=" + long + "&appid=752b41def39260e71140c4018e9a0f11"
 
         $.ajax({
@@ -98,6 +105,7 @@ function updateForecast () {
             var help = dateInfo.split("-");
             var uvId = document.getElementById("uv-index");
 
+            //else if tree to make sure correct color is displayed. The graphic I got on google images said: <=2 then low, 2-7 med/high, 7+ is severe
             if (uV <= 2)    {
                 $("#uv-index").empty();
                 $("#uv-index").removeClass();
@@ -139,6 +147,7 @@ function updateForecast () {
     });
 }
 
+//function for building the 5 day schedule using a seperate api that had to be iterated through due to it being every 3 hours
 function fiveDaySchedule () {
     var urlFiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + api_key;
     $.ajax({
@@ -146,10 +155,12 @@ function fiveDaySchedule () {
         method: "GET"
     }).then(function(pageInfo) {
 
+        //removing old blocks
         for (var i = 1; i <6; i++) {
             $("#daily-block"+i).remove();
         }
         
+        //for loop for adding in new blocks
         for (var i = 1; i <6; i++) {
 
             //all of the variables for the week
